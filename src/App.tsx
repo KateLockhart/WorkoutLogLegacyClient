9dcf6df5f1b08dd685ed0168b0ec193cdf949412
KateLockhart <katelockhart707@gmail.com>
2021-08-06 12:11:22 -0400

    Added comments for logic needed to impliment in App.tsx while introducing primary TS typed props and state.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,57 @@
+import React from 'react';
 import './App.css';
 import SiteBar from './home/Navbar';
-import Auth from './components/authFunctionality/Auth';
+import Auth from './components/auth/Auth';
+
+type AppProps = {
+  password: string;
+  username: string;
+  token: string;
+};
+
+type AppState = {
+  setPassword: string;
+  setUsername: string;
+  setToken: string;
+};
+
+//  Using TS with Legacy, React.Component takes in three generic types(can view w/ tooltip), but we focus on the first two:
+//  React.Component<P , S> where P is for prop types and S is for state types
+
+export default class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps){
+    super(props);
+    this.state = {
+      setPassword: '',
+      setUsername: '',
+      setToken: '',
+    };
+  }
+
+  // give definition
+  componentWillMount() {
+    console.log('')
+  }
+
+  // give definition
+  componentDidMount() {
+
+  }
+
+  // make update token(setting the token) logic
+
+  // make clearing token logic
+
+  // make the logic for the protected view(auth vs workouts) and call in return
 
 
-const App: React.FunctionComponent = () => {
-  
+  render() {
+    return (
+      <div className="App">
+        <SiteBar />
+        <Auth />
+      </div>
+    );
+  }
 
-  return (
-    <div className="App">
-      <SiteBar/>
-      <Auth/>
-    </div>
-  );
 }
-
-export default App;
